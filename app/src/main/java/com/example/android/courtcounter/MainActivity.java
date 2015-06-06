3c0912369f111123a94d1eb9e211f474465c0c8a
tommyputranto <tommyputranto1@gmail.com>
2015-06-06 18:14:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.android.courtcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /**
+     * Displays the given score for 3 point button A.
+     */
+    public void tigapointA(View view){
+        scoreTeamA = scoreTeamA+3;
+        displayForTeamA(scoreTeamA);
+    }
+    /**
+     * Displays the given score for 2 point button A.
+     */
+    public void duapointA(View view){
+        scoreTeamA = scoreTeamA+2;
+        displayForTeamA(scoreTeamA);
+        displayForTeamA(scoreTeamA);
+    }
+    /**
+     * Displays the given score for free throw button A.
+     */
+    public void freeA(View view){
+        scoreTeamA = scoreTeamA+1;
+        displayForTeamA(scoreTeamA);
+        displayForTeamA(scoreTeamA);
+    }
+
+
+    /**
+     * Displays the given score for 3 point button B.
+     */
+    public void tigapointB(View view){
+        scoreTeamB = scoreTeamB+3;
+        displayForTeamB(scoreTeamB);
+    }
+    /**
+     * Displays the given score for 2 point button B.
+     */
+    public void duapointB(View view){
+        scoreTeamB = scoreTeamB+2;
+        displayForTeamB(scoreTeamB);
+        displayForTeamB(scoreTeamB);
+    }
+    /**
+     * Displays the given score for free throw button A.
+     */
+    public void freeB(View view){
+        scoreTeamB = scoreTeamB+1;
+        displayForTeamB(scoreTeamB);
+        displayForTeamB(scoreTeamB);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+}
